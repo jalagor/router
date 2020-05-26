a292ac883e6f9499f3b04cea15535a062a1eaf1b
jalagor <johnrossrogla@gmail.com>
2020-05-25 23:53:12 -0600

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+      <Router> 
+      <div className="App">
+        {/*switch returns the first route that matches path */}
+         <Route exact path="/" component={Home} /> {/* exact blocks the slash matching /todos */}
+        <Route path="/todos" component={Todos}/>
+        <Route path="/about">
+          <About name="John"/>
+        </Route>
+        
+  
+      </div>
+      </Router>
+    );
+  }
+}
+
+function Todos(props){
+  return(
+    <ul>
+      <li>a Todo</li>
+    </ul>
+  )
+}
+
+function About(props){
+  return <h1>hi! {props.name}</h1>
+
+}
+
+function Home(props){
+  return(
+    <h1>app</h1>
+  )
 }
 
 export default App;
