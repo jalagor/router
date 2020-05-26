fec5a3c78e70b8d7ec92ec1db8f5a8379e47cc29
jalagor <johnrossrogla@gmail.com>
2020-05-25 23:55:41 -0600

    revisit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
+// revisit
 
 class App extends Component{
   render(){
